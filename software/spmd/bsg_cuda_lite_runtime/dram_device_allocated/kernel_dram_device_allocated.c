659c407a93bde718a1abc2d943e2892f8c2a70f3
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-06-21 23:55:08 +0000

    1. Changed cuda lite runtime test test_dram to test_dram_device_allcoated 2. Added a smaller cuda lite runtime regression test called test_dram_host_allocated

--- software/spmd/bsg_cuda_lite_runtime/dram_device_allocated/kernel_dram_device_allocated.c
+++ software/spmd/bsg_cuda_lite_runtime/dram_device_allocated/kernel_dram_device_allocated.c
@@ -12,7 +12,7 @@ INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X-1, 0, bsg_tiles_Y-1
 
 volatile int A[N] __attribute__((section(".dram")));
 
-int  __attribute__ ((noinline)) kernel_dram(int *addr, int block_size_x) {
+int  __attribute__ ((noinline)) kernel_dram_device_allocated(int *addr, int block_size_x) {
 
 	int start_x = block_size_x * (__bsg_tile_group_id_y * __bsg_grid_dim_x + __bsg_tile_group_id_x); 
 	for (int iter_x = __bsg_id; iter_x < block_size_x; iter_x += bsg_tiles_X * bsg_tiles_Y) { 
